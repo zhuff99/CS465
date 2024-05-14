bfc3b00be1537a5d82af37a0e851814b723dfd74
Zachary Huff <zhuff99@gmail.com>
2024-05-14 17:37:07 +0800

    Module 2 Completed baseline

---
+++ app_server/routes/travel.js
@@ -0,0 +1,8 @@
+var express = require('express')
+var router = express.Router();
+var controller = require('../controllers/travel');
+
+router.get('/', controller.travel);
+
+module.exports = router;
+

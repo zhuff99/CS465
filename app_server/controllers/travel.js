764b5327f3787c50efc49d1fc5d099d7af462c2e
Zachary Huff <zhuff99@gmail.com>
2024-05-26 18:58:30 -0700

    Module 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,4 +1,7 @@
 /* GET travel view */
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('data/trips.json','utf8'));
+
 
 const travel = (req, res) => {
     res.render('travel', {title: 'Travlr Getaways'});
